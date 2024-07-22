62fc3a7d68f6d60ec268429bd05d99e3c0ee4ec9
shivaraj <shivaraj.feb@gmail.com>
2024-07-22 20:46:43 +0530

    new changes

--- src/containers/login/v1/index.tsx
+++ src/containers/login/v1/index.tsx
@@ -1,3 +1,14 @@
-export default function login() {
-  return <div>login page from v1</div>;
+import React from 'react';
+import { Col, Row } from 'antd';
+import styles from './loginV1.module.css';
+
+const Login =() => {
+  return (
+    <Row className={styles.loginContainerV1}>
+      <Col span={16} className={styles.loginSideV1}>image</Col>
+      <Col span={8}>login from v1</Col>
+    </Row>
+  );
 }
+
+export default Login;
